94d3d4b28bc613a78a7ce558c1ad684f32538a8b
SwathiPriya2000 <63891711+SwathiPriya2000@users.noreply.github.com>
2020-09-08 17:15:38 +0530

    Add files via upload

---
+++ Loopchar.java
@@ -0,0 +1,18 @@
+class Loopchar {
+  public static void main(String[] args) {
+
+    // create a string
+    String name = "Programmer";
+
+    System.out.println("Characters in " + name + " are:");
+
+    // loop through each element
+    for(int i = 0; i<name.length(); i++) {
+
+      // access each character
+      char a = name.charAt(i);
+      System.out.print(a + ", ");
+    }
+  }
+}
+
